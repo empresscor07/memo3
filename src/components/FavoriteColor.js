d7a683218c9c49b753c72288fd4120010e153364
yorkmac033 <empresscor07>
2021-11-23 16:54:05 -0600

    Using useState and React-bootstrap to play with color buttons

--- src/components/FavoriteColor.js
+++ src/components/FavoriteColor.js
@@ -1,28 +1,42 @@
 import React, { useState } from "react";
+import {Badge, Button} from "react-bootstrap";
 
 function FavoriteColor() {
     const [color, setColor] = useState("red");
+    const [bg, setBg] = useState("danger")
 
     return (
         <>
             <h1>useState</h1>
-            <h2>My favorite color is {color}!</h2>
-            <button
+            <h2>My favorite color is <Badge bg={bg}>{color}</Badge></h2>
+            <Button
+                className='mx-2'
+                variant='primary'
                 type="button"
-                onClick={() => setColor("blue")}
-            >Blue</button>
-            <button
+                onClick={() => {setColor("blue")
+                setBg("primary")}}
+            >Blue</Button>
+            <Button
+                className='mx-2'
+                variant='danger'
                 type="button"
-                onClick={() => setColor("red")}
-            >Red</button>
-            <button
+                onClick={() => {setColor("red")
+                setBg("danger")}}
+            >Red</Button>
+            <Button
+                className='mx-2'
+                variant='warning'
                 type="button"
-                onClick={() => setColor("pink")}
-            >Pink</button>
-            <button
+                onClick={() => {setColor("yellow")
+                setBg("warning")}}
+            >Yellow</Button>
+            <Button
+                className='mx-2'
+                variant='success'
                 type="button"
-                onClick={() => setColor("green")}
-            >Green</button>
+                onClick={() => {setColor("green")
+                setBg("success")}}
+            >Green</Button>
         </>
     );
 }
